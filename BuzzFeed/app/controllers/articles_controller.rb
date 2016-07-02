442faa164e8a16df56b555a9f34a2dbdab803cd9
David Gwalthney <theblob2005@yahoo.com>
2016-07-02 17:23:46 -0400

    fixed name for create article

--- BuzzFeed/app/controllers/articles_controller.rb
+++ BuzzFeed/app/controllers/articles_controller.rb
@@ -70,6 +70,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :body, :image_url)
+      params.require(:article).permit(:title, :body, :image_url, :name)
     end
 end
