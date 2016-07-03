3c2241d6d4013bf1796f826ba38834ca9c3cda68
David Gwalthney <theblob2005@yahoo.com>
2016-07-03 00:48:15 -0400

    updated tab title, no <p> in database

--- BuzzFeed/db/seeds.rb
+++ BuzzFeed/db/seeds.rb
@@ -9,7 +9,7 @@
 Article.delete_all
 Article.create!( 
   title:  "<i>Wreck-It Ralph 2 officially announced at Disney</i>",
-  byline: "<p><b>He’s gonna wreck it! Again!</b>",
+  byline: "<b>He’s gonna wreck it! Again!</b>",
   body: "<p>
          Disney announced Thursday that a sequel to its 2012 film Wreck-It Ralph is officially in
          development over at the studio’s animation arm, slated for release on March 9, 2018.
@@ -23,7 +23,7 @@ Article.create!(
   image_url: "wreck-it-ralph.jpg",
   category: "movies",
   category2: "games",
-  name: "<p>booye" )
+  name: "booye" )
   
 Article.create!( 
   title:  "<i>Philippe Cousteau Discovers Sharks off Islands Formerly Used for Nuclear Testing</i>",
@@ -39,4 +39,4 @@ Article.create!(
   image_url: "sharkweek.jpg",
   category: "science",
   category2: "",
-  name: "<p>david" )
+  name: "david" )
