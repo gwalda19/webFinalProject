69bf27b85aa4d304510e775a13f7a3a30fe91303
David Gwalthney <theblob2005@yahoo.com>
2016-07-04 22:20:02 -0400

    games pic button working

--- BuzzFeed/config/routes.rb
+++ BuzzFeed/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :articles
   get 'admin'  => 'admin#index'
+  get 'games'  => 'games_feed#index', as: :games
   get 'music'  => 'music_feed#index', as: :music
   get 'movies' => 'movies_feed#index', as: :movies
 
