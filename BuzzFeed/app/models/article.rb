791d681d345665611b4f8a7604ac45e0983a8d07
Christopher Booye <chris.booye@gmail.com>
2016-07-05 22:59:43 -0400

    forces the category and category2 fields to be lowercase, no matter what the user enters.

--- BuzzFeed/app/models/article.rb
+++ BuzzFeed/app/models/article.rb
@@ -1,5 +1,18 @@
 class Article < ActiveRecord::Base
   
+  validates :title, :image_url, :body, :name, :byline, :category, :presence => true
+ 
+  before_validation :lowercase_category
+  before_validation :lowercase_category2
+
+  def lowercase_category
+    category.downcase!
+  end
+ 
+   def lowercase_category2
+    category2.downcase!
+  end
+ 
   def self.latest
     Article.order(:updated_at).last
   end
